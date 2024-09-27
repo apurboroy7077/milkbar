fbd0fb80f434a2803fc8e578552af77f65866d8c
PhantomGuru <103819634+PhantomGuru0427@users.noreply.github.com>
2024-09-27 21:12:31 +0800

    feat: add video & sticky header

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,24 +1,25 @@
 import React from "react";
-import BannerImage from '../assets/images/home-banner.jpg';
-import BannerMobileImage from '../assets/images/home-banner-mobile.jpg';
+import BannerImage from "../assets/images/home-banner.jpg";
+import BannerMobileImage from "../assets/images/home-banner-mobile.jpg";
+import HomeBackgroundVideo from "../assets/videos/milkbarHomeDesktop.mp4";
 
 function HomeBanner() {
-    return (
-        <div className="main_home_banner page_banner position-relative">
-            <picture>
-                <source media="(max-width: 645px)" srcSet={BannerMobileImage} />
-                <img src={BannerImage} alt="Logo" />
-            </picture>
-            <div className="main_home_banner_content position-absolute w-100 h-100">
-            </div>
-            <div className="bannerbox">
-                {/* <img src={BannerImage} alt="Logo" style={{ width: '100%', position: "absolute", height:  }} /> */}
-                <div className="home_banner_bottom text-light position-relative">
-                    <h1 className="text-white m-0 text-center">branding, design, dev for luxury brands</h1>
-                </div>
-            </div>
+  return (
+    <div className="main_home_banner page_banner position-relative">
+      <video autoPlay muted loop playsInline className="main_home_banner_video">
+        <source src={HomeBackgroundVideo} type="video/mp4" />
+      </video>
+      <div className="main_home_banner_content position-absolute w-100 h-100"></div>
+      <div className="bannerbox">
+        {/* <img src={BannerImage} alt="Logo" style={{ width: '100%', position: "absolute", height:  }} /> */}
+        <div className="home_banner_bottom text-light position-relative">
+          <h1 className="text-white m-0 text-center">
+            branding, design, dev for luxury brands
+          </h1>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default HomeBanner;
