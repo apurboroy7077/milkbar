d82baa99fada41ce5e962ec3d3549e226a4076f1
Apurbo Roy <apurboroy7077@gmail.com>
2024-10-09 15:16:21 +0600

    updated hero section of work page

--- src/pages/AboutPage2.jsx
+++ src/pages/AboutPage2.jsx
@@ -58,7 +58,7 @@ const AboutPage2 = () => {
             <div className=" absolute top-0 w-full h-full">
               <div className="h-full">
                 <div className="w-full h-full flex ">
-                  <div className=" w-[60%] xl:w-[65%] bg-[black] bg-opacity-50 backdrop-blur-sm h-full ">
+                  <div className=" w-[60%] xl:w-[50%] bg-[black] bg-opacity-50 backdrop-blur-sm h-full ">
                     <div className="flex justify-end items-center h-full">
                       <div className="px-10 xl:pl-16 xl:pr-[175px] py-10 w-[90%] lg:w-[80%] xl:w-[80%]">
                         <div>
@@ -85,7 +85,7 @@ const AboutPage2 = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="w-[40%] xl:w-[35%] h-full"></div>
+                  <div className="w-[40%] xl:w-[50%] h-full"></div>
                 </div>
                 <div className="absolute top-0 w-full">
                   <div className="py-10">
