78e30885d914d45a02daa0a0467a33a33f6dfe98
PhantomGuru <103819634+PhantomGuru0427@users.noreply.github.com>
2024-10-11 17:50:47 +0800

    update

--- src/data/variables/variables-1.js
+++ src/data/variables/variables-1.js
@@ -1,2 +1,2 @@
-export const BACKEND_SERVER_ADDRESS = `http://54.237.212.153`;
+export const BACKEND_SERVER_ADDRESS = `http://54.237.212.153:5000`;
 export const GET_EVERY_PROJECT_DATA_API_ADDRESS = `${BACKEND_SERVER_ADDRESS}/api/admin/get-projects`;
