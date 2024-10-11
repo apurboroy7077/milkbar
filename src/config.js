e7918f8474b0cf77707f0da4fbde1e2c756b4a0a
PhantomGuru <103819634+PhantomGuru0427@users.noreply.github.com>
2024-10-11 17:33:13 +0800

    fix: update server address

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  BASE_URL: "http://45.8.22.219:5000", // Use the actual base URL of your API
+  BASE_URL: "http://54.237.212.153:5000", // Use the actual base URL of your API
   // BASE_URL: "http://localhost:5000",  // Use the actual base URL of your API
 };
 
--- src/data/variables/variables-1.js
+++ src/data/variables/variables-1.js
@@ -1,2 +1,2 @@
-export const BACKEND_SERVER_ADDRESS = `http://localhost:5000`;
+export const BACKEND_SERVER_ADDRESS = `http://54.237.212.153`;
 export const GET_EVERY_PROJECT_DATA_API_ADDRESS = `${BACKEND_SERVER_ADDRESS}/api/admin/get-projects`;
